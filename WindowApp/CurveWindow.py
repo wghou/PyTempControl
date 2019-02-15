27b2503b1ce276a5aae80919a334570ea4c20cc3
wghou <wghou09@bjtu.edu.cn>
2019-02-15 17:27:27 +0800

    basic functions have been finished

---
+++ WindowApp/CurveWindow.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'CurveWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_CurveWindow(object):
+    def setupUi(self, CurveWindow):
+        CurveWindow.setObjectName("CurveWindow")
+        CurveWindow.resize(583, 429)
+
+        self.retranslateUi(CurveWindow)
+        QtCore.QMetaObject.connectSlotsByName(CurveWindow)
+
+    def retranslateUi(self, CurveWindow):
+        _translate = QtCore.QCoreApplication.translate
+        CurveWindow.setWindowTitle(_translate("CurveWindow", "Form"))
+
